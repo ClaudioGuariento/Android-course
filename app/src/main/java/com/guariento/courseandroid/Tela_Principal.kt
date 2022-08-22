ad21499f9ebb15e48219d3019b2ea80c1e354742
Claudio Cesar Guariento <claudiog@motorola.com>
2022-08-22 20:56:03 -0300

    tela com binding

---
+++ app/src/main/java/com/guariento/courseandroid/Tela_Principal.kt
@@ -0,0 +1,11 @@
+package com.guariento.courseandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tela_Principal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_principal)
+    }
+}
---
+++ app/src/main/java/com/guariento/courseandroid/Usuario.kt
@@ -0,0 +1,7 @@
+package com.guariento.courseandroid
+
+class Usuario(
+    val nome: String,
+    val email:String,
+    val senha:String
+){}
